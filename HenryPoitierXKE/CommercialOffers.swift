a9ec44d8e32f323e183cf791c412308d49df2fe9
Maxime Eglem <maxime.eglem@gmail.com>
2017-06-16 09:55:46 +0200

    add commercial Model

---
+++ HenryPoitierXKE/CommercialOffers.swift
@@ -0,0 +1,34 @@
+//
+//  CommercialOffers.swift
+//  HenryPoitierXKE
+//
+//  Created by Maxime Eglem on 15/06/2017.
+//  Copyright © 2017 Xebia IT Architects. All rights reserved.
+//
+
+import Foundation
+import Unbox
+
+struct Offers {
+    let offers: [Offer]
+}
+
+extension Offers: Unboxable {
+    init(unboxer: Unboxer) throws {
+        self.offers = try unboxer.unbox(key: "offers")
+    }
+}
+
+struct Offer{
+    let type: String
+    let value: Double
+    let sliceValue: Double?
+}
+
+extension Offer: Unboxable {
+    init(unboxer: Unboxer) throws {
+        self.type = try unboxer.unbox(key: "type")
+        self.value = try unboxer.unbox(key: "value")
+        self.sliceValue = unboxer.unbox(key: "sliceValue")
+    }
+}
